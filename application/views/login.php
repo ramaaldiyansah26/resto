98d91737408d1910f6dbce18ad223d84a2864829
faridassidqi <bolotok77kongking@gmail.com>
2020-03-10 09:27:21 +0700

    uploand pertama

---
+++ application/views/login.php
@@ -0,0 +1,56 @@
+
+</head>
+<link rel="stylesheet" type="text/css" href="assets/dist/css/bootstrap.css">
+<body>
+  <style type="text/css">
+    .panel-warning > .panel-heading {
+    color: #8a6d3b;
+    background-color: #5cb85c;
+    border-color: #faebcc;
+}
+  </style>
+
+<br><br>
+    <div class="container">
+      <center>
+      <div class="col-md-5 col-md-offset-3">
+        <div class="panel panel-warning">
+          <div class="panel-heading">
+            <h3> <span  aria-hidden="true"></span>Resto Mona</h3>
+            <p> <span  aria-hidden="true"></span> </p>
+    
+              </div>
+                <div class="panel-body">
+
+      
+                  <div class="col-md-11">
+
+                  <form method="post">
+
+
+                    <div class="input-group">
+                    <span class="input-group-addon" id="basic-addon1">Username</span>
+                    <input type="text" name="user" class="form-control" placeholder="username" aria-describedby="basic-addon1" required="required">
+                  </div>
+                  <br>
+                  <div class="input-group">
+                    <span class="input-group-addon" id="basic-addon1">Password</span>
+                    <input type="password" name="pass" class="form-control" placeholder="password" aria-describedby="basic-addon1" required="required">
+                  </div>
+                  <br>
+                  <div class="input-group">
+                    <span class="input-group-addon" id="basic-addon1">Nama User</span>
+                    <input type="password" name="pass" class="form-control" placeholder="nama user" aria-describedby="basic-addon1" required="required">
+                  </div>
+                  <br>
+                  </div>
+                    <input type="submit" class="btn btn-block btn-success" value="Login">
+                  
+                  </form>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
+
+</body>
+</html>
